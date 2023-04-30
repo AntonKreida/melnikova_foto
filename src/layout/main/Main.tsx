c0633f41c566120276ed0e07850fd2b4624373ab
Kreida Anton <kreida.anton@yandex.ru>
2023-04-30 23:17:14 +0500

    index | fixed sidebar and main

--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -8,7 +8,7 @@ interface IProps {
 }
 
 export const Main: FC<IProps> = ({ children }) => (
-  <main className="flex h-full w-full flex-col overflow-hidden pb-5 pl-12 pt-40">
+  <main className="ml-[25%] flex h-full w-full flex-col overflow-hidden pb-5 pl-12 pt-40">
     <div className="flex h-full flex-col gap-16">
       <Header title="Профессиональная фотосъёмка в Тюмени" />
       { children }
--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -7,11 +7,11 @@ import { Contacts } from './contacts';
 
 
 export const Sidebar = () => (
-  <aside className="h-screen w-2/5 overflow-y-scroll shadow-custom scrollbar-hide">
-    <div className="flex h-full w-full px-16 pb-5 pt-32 md:px-8">
-      <div className="flex flex-col justify-between md:gap-12">
+  <aside className="fixed h-screen w-[25%] overflow-y-scroll px-16 pb-5 pt-32 shadow-custom scrollbar-hide md:px-8">
+    <div className="flex h-auto w-full">
+      <div className="flex h-auto w-full flex-col justify-between md:gap-12">
 
-        <div className="flex flex-col gap-16 md:gap-8">
+        <div className="flex h-full w-full flex-col gap-16 md:gap-8">
           <Link className="flex flex-col gap-2" href="/">
             <h1 className="font-default text-lg uppercase text-black+ md:text-md">
               Александра  <br />Мельникова
