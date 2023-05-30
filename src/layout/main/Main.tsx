cc2db76e5c3f1a7b57fc4b615c653f7bc64f44bc
Kreida Anton <kreida.anton@yandex.ru>
2023-05-30 16:22:19 +0500

    lookbook | add button up

--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import { useRouter } from 'next/router';
 
 import { headerTitles, IHeaderTitles } from '@lib';
+import { ButtonUp } from '@ui-kit';
 
 import { Header } from '../header';
 
@@ -26,12 +27,18 @@ export const Main: FC<IProps> = ({ children }) => {
     return false;
   };
 
+  const handlerClickUpScrollPage = () => {
+    console.log('клик');
+  };
+
   return (
     <main className="ml-[25%] flex h-full w-full flex-col overflow-hidden pb-14 pl-12 pt-40">
-      <div className="flex h-full flex-col gap-16">
+      <div className="relative flex h-full flex-col gap-16">
         <Header path={ getNormalCase() } subtitle={ titles.subtitle } title={ titles.title } />
         { children }
+        <ButtonUp text="Назад" onClick={ handlerClickUpScrollPage } />
       </div>
+
     </main>
   );
 };
--- src/ui-kit/buttonUp/buttonUp.tsx
+++ src/ui-kit/buttonUp/buttonUp.tsx
@@ -1,11 +1,22 @@
 import React, { FC } from 'react';
 
+import UndoIcon from '@public/assets/icon/undo.svg';
+
 
 interface IProps {
   onClick: React.MouseEventHandler;
+  text: string;
 }
 
 
-export const ButtonUp: FC<IProps> = ({ onClick }) => (
-  <button type="button" onClick={ onClick } />
+export const ButtonUp: FC<IProps> = ({ onClick, text }) => (
+  <button
+    className="group absolute right-8 top-0 flex w-fit translate-y-[50%] cursor-pointer items-center gap-1 font-med uppercase text-gray+ transition-all hover:text-gray-400 active:scale-[0.8]"
+    type="button"
+    onClick={ onClick }
+  >
+    { text }
+    <UndoIcon className="h-[20px] w-[20px] fill-gray+ transition-all group-hover:fill-gray-400" />
+  </button>
+
 );
